c8bed1f85ff2d578966249ad313132cee322d424
Shaban Samani <104533193+NabeelHassanali@users.noreply.github.com>
2024-04-29 14:14:14 -0400

    Section 12: Flyway Migrations

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -27,6 +27,9 @@ public class Customer {
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
+    @Column(length = 255)
+    private String email;
+
     @Version
     private Integer version;
     private LocalDateTime createdDate;

aa4c3c6f21ccd9f3851d16e7966861c41e78649f
Shaban Samani <shabansamani@gmail.com>
2024-05-06 18:54:52 -0400

    Finished Section 15

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -70,7 +71,9 @@ public class BeerServiceJPA implements BeerService {
             queryPageSize = DEFAULT_PAGE_SIZE;
         }
 
-        return PageRequest.of(queryPageNumber, queryPageSize);
+        Sort sort = Sort.by(Sort.Order.asc("beerName"));
+
+        return PageRequest.of(queryPageNumber, queryPageSize, sort);
     }
 
     protected Page<Beer> listBeersByNameAndStyle(String beerName, BeerStyle beerStyle, Pageable pageable) {

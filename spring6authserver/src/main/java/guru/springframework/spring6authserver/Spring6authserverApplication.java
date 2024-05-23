78cb3fe33db4fa0571fbedf0ace0d05c2f9d7428
Shaban Samani <shabansamani@gmail.com>
2024-05-23 12:34:56 -0400

    Boilerplate for Spring 6 Auth Server

---
+++ spring6authserver/src/main/java/guru/springframework/spring6authserver/Spring6authserverApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6authserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6authserverApplication.class, args);
+	}
+
+}

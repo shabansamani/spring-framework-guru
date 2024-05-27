d947ca76f93c69151f5cd9dfdb5ef3a215ae8e44
Shaban Samani <shabansamani@gmail.com>
2024-05-27 17:07:12 -0400

    Boilerplate for Section 25: Reactive Programming

---
+++ spring6reactive/src/main/java/guru/springframework/spring6reactive/Spring6reactiveApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6reactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6reactiveApplication.class, args);
+	}
+
+}

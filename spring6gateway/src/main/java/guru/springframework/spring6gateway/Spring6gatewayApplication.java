9849383ae6c386c569c53b5466aa48bd1f3b7da6
Shaban Samani <shabansamani@gmail.com>
2024-08-04 15:56:45 -0400

    OAuth2 w/ Spring Web Client and Spring Cloud Gateway

---
+++ spring6gateway/src/main/java/guru/springframework/spring6gateway/Spring6gatewayApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6gatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6gatewayApplication.class, args);
+	}
+
+}

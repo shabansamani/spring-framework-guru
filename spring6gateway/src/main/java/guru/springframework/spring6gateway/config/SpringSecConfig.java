966bc83d8ee75f30bdf30f491a8a3996f150c530
Shaban Samani <shabansamani@gmail.com>
2024-08-05 15:13:39 -0400

    Section 36: Spring Cloud Gateway

---
+++ spring6gateway/src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecConfig {
+    @Bean
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+            .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+            .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return http.build();
+    }
+}
